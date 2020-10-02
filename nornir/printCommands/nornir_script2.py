d0071d619bed910f81a3a5204046787e1c6e570f
wkwiat <wkwiatjr@me.com>
2020-10-02 15:21:05 -0600

    first commit

---
+++ nornir/printCommands/nornir_script2.py
@@ -0,0 +1,23 @@
+from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_command
+from nornir_utils.plugins.functions import print_result
+import colorama
+from colorama import Fore, Style
+
+print("\n")
+print(Fore.YELLOW + "#" * 70 + Style.RESET_ALL)
+print(" " * 20 + "Welcome to " + Fore.RED + "Nornir!" + Style.RESET_ALL)
+print(" " * 15 + "This is a Dynamic script!")
+print("\n Please enter the commands you wish to execute, separated by commas")
+print("Example: " + Fore.GREEN + "< show ip int brief, show ip route, show version >")
+print(Fore.YELLOW + "#" * 70 + Style.RESET_ALL)
+commands = input ("\nEnter Commands: ")
+cmds = commands.split(",")
+
+for cmd in cmds:
+    nr = InitNornir()
+    result = nr.run(
+        task=netmiko_send_command,
+        command_string=cmd
+        )
+    print_result(result)
